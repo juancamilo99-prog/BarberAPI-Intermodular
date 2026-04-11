88b01ff67832ba71d55c84dbbfaf5cf717efb2df
Jheremi Lema <jheremisj9695@gmail.com>
2026-04-11 20:22:56 +0200

    Creación de la clase conexión base de datos BarberApi

--- barberapi/src/main/java/com/example/barberapi/BarberapiApplication.java
+++ barberapi/src/main/java/com/example/barberapi/BarberapiApplication.java
@@ -1,13 +1,26 @@
 package com.example.barberapi;
 
+import com.example.barberapi.database.DBConecction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class BarberapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BarberapiApplication.class, args);
-	}
+
+		//Probamos Conexión a Base de datos
+		Connection connection = DBConecction.getConnection();
+
+        try {
+            System.out.println("Se ha realizado la conexión a la DB: " + connection.getCatalog());
+        } catch (SQLException e) {
+			System.out.println(e.getMessage());
+        }
+    }
 
 }
--- barberapi/src/main/java/com/example/barberapi/database/DBConecction.java
+++ barberapi/src/main/java/com/example/barberapi/database/DBConecction.java
@@ -1,4 +1,39 @@
 package com.example.barberapi.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConecction {
+
+    //Conexión a DB
+    //Patrón Singleton
+    private static Connection connection;
+
+    public static Connection getConnection(){
+
+        if ( connection == null){
+            createConnection();
+        }
+
+        return connection;
+    }
+
+    private static void createConnection(){
+
+        //Parámetros de Conexión
+        String user = "root";
+        String pass = "root";
+        String database = "barberapi";
+
+        //Creamos Conexión
+
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + database, user, pass);
+        } catch (SQLException e) {
+            System.out.println("Error: No se pudo establecer conexión con DB");
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
