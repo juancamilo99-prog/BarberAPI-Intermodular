848c6820c67b719b925de1e881bf13ed0638b07f
monterocamilo99@hotmail.com <monterocamilo99@hotmail.com>
2026-04-23 12:30:35 +0200

    creacion clientes formulario e insercion en la bd

---
+++ barberapi/src/main/java/com/example/barberapi/controller/ClientesController.java
@@ -0,0 +1,39 @@
+package com.example.barberapi.controller;
+
+import com.example.barberapi.dao.ClientesDAO;
+import com.example.barberapi.model.Clientes;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class ClientesController {
+
+    private ClientesDAO clientesDAO;
+    private List<Clientes> listaClientes;
+
+    public ClientesController() {
+        clientesDAO = new ClientesDAO();
+    }
+
+    public long addClientes(Clientes clientes) throws Exception{
+        //agregamos un usuario
+        if (clientes.getNombre() == null || clientes.getNombre().trim().isEmpty()){
+            throw new Exception("El nombre del cliente es obligatorio");
+        }
+        if (clientes.getApellido() == null || clientes.getApellido().trim().isEmpty()){
+            throw new Exception("El apellido del cliente es obligatorio");
+        }
+        if (clientes.getTelefono() == null || clientes.getTelefono().trim().isEmpty()){
+            throw new Exception("El telefono del cliente es obligatorio");
+        }
+        if (clientes.getCorreo() == null || clientes.getCorreo().trim().isEmpty()){
+            throw new Exception("El correo del cliente es obligatorio");
+        }
+        try {
+            return clientesDAO.insertarClientes(clientes);
+        } catch (SQLException e) {
+            throw new Exception("Error al guardar el cliente en la BD");
+        }
+
+    }
+}
